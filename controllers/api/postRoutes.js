865fafb95525f0fcfb834e6d1a5f4884087109a8
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-23 19:10:41 -0600

    fixing post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
         });
         res.status(200).json(postData);
     } catch (err) {
-        res.status(500).json(err);        
+        res.status(500).json(err);
     }
 });
 
